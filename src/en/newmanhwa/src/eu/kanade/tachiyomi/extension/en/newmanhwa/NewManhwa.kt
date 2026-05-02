3edd068b004437674f90b7cbc09bd6c442489c1d
rzc2333 <rzc2333@outlook.com>
2026-05-02 16:28:25 +0800

    Delete src/en directory

--- src/en/newmanhwa/src/eu/kanade/tachiyomi/extension/en/newmanhwa/NewManhwa.kt
+++
@@ -1,5 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.newmanhwa
-
-import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
-
-class NewManhwa : MangaThemesia("New Manhwa", "https://newmanhwa.com", "en")
